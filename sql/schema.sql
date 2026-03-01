2948493c1bd9af525957ae00195e653410c26457
Zaur İsrafilov <israfilovzaur1@gmail.com>
2026-03-01 14:33:26 +0400

    Delete sql/schema.sql

--- sql/schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    first_name VARCHAR(50),
-    last_name VARCHAR(50),
-    email VARCHAR(100) UNIQUE,
-    country VARCHAR(50),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
